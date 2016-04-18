f8fddd40a37ce483d81f12aaaca9f0e82af63e83
fanjunwei <fanjunwei003@163.com>
2016-04-18 20:56:49 +0800

    配置文件

--- sub/release/www/js/base/config_ex.js
+++ sub/release/www/js/base/config_ex.js
@@ -1,6 +1,8 @@
 /**
- * Created by fanjunwei on 16/4/18.
+ * Created by fanjunwei on 16/1/11.
  */
 (function (window) {
     var base_config = window.base_config || {};
+    base_config.base_url = "";
 })(window);
+
--- www/js/base/config.js
+++ www/js/base/config.js
@@ -1,3 +1,10 @@
 /**
  * Created by fanjunwei on 16/4/18.
  */
+`(function (window) {
+    var base_config = window.base_config = {};
+    base_config.base_url = "";
+    base_config.is_develop = false;
+    base_config.is_debug = false;
+    base_config.channel = "desktop";
+})(window);

cee44c1a7e27e1c743750b71662093a4723a11d9
fanjunwei <fanjunwei003@163.com>
2016-04-28 21:40:39 +0800

    修改配置

--- www/base/config.js
+++ www/base/config.js
@@ -9,7 +9,7 @@
     base_config.is_debug = true;
     base_config.channel = "desktop";
     base_config.mqtt = {
-        host: "mqtt://0.0.0.0:1884",
+        host: "mqtt://121.42.211.17:1884",
         client_type: "web"
     }
 })(window);
